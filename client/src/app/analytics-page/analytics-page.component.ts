d6e4dc407977e78df5c5733e8d55d25f16e037b9
advert31337 <advert31337@yandex.ru>
2020-01-25 16:25:45 +0300

    first

---
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -0,0 +1,38 @@
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core'
+import {AnalyticsService} from '../shared/services/analytics.service'
+import {AnalyticsPage} from '../shared/interfaces'
+import {Subscription} from 'rxjs'
+
+@Component({
+  selector: 'app-analytics-page',
+  templateUrl: './analytics-page.component.html',
+  styleUrls: ['./analytics-page.component.css']
+})
+export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('gain') gainRef: ElementRef
+  @ViewChild('order') orderRef: ElementRef
+
+  aSub: Subscription
+  average: number
+  pending = true
+
+  constructor(private service: AnalyticsService) {
+  }
+
+  ngAfterViewInit() {
+    this.aSub = this.service.getAnalytics().subscribe((data: AnalyticsPage) => {
+      this.average = data.average
+
+
+      this.pending = false
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.aSub) {
+      this.aSub.unsubscribe()
+    }
+  }
+
+}
